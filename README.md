46de245664a0cafc8e3f180f95369b4d2af4824b
justan0therdave <justanotherdavemartin@gmail.com>
2018-10-14 21:04:21 +0100

    added block expression

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Handlebars playground
 
-Templating fun with [Handlebars](http://handlebarsjs.com)
+Templating fun with [Handlebars](http://handlebarsjs.com) made with [Express Handlebars](https://github.com/ericf/express-handlebars)
 
 ##
 
@@ -8,4 +8,10 @@ Handlebars expressions:
 * use double curly-braces: `{{ content }}`
 * can contain dot-seperated paths: `{{ article.title }}`
 * cannot contain any of: `whitespace` `!` `"` `#` `%` `&` `'` `(` `)` `*` `+` `,` `.` `/` `;` `<` `=` `>` `@` `[` `\` `]` `^` `{` `|` `}` `~`
-* can use triple curly-braces: `{{{body}}}` if you don't want to escape an expression
+* can use triple curly-braces: `{{{body}}}` if you don't want to escape an expression.
+
+Block expressions:
+* can be defined using helpers which change the context
+* the helper receives an options hash which has a function `options.fn` that behaves like a normal handlebars template
+* Identified with a `#` and has a closing 'mustache' of the same name preceded with `/`
+`{{#list people}}{{firstName}} {{lastName}}{{/list}}`
--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express');
 const expresshbs = require('express-handlebars');
 const app = express();
 
+// Register Handlebars helpers
+const hbs = expresshbs.create({
+  defaultLayout: 'main', 
+  extname: '.hbs',
+  helpers: {
+    'list': (items, options) => {
+      let out = "<ul>";
+      for(let i=0, l=items.length; i<l; i++) {
+        out = out + "<li>" + options.fn(items[i]) + "</li>";
+      }
+      return out + "</ul>";
+    }
+  }
+})
+    
+
 // Register Handlebars view engine
-app.engine('.hbs', expresshbs({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('.hbs', hbs.engine)
 // Use hbs view engine
 app.set('view engine', '.hbs')
 
@@ -12,7 +28,12 @@ app.get('/', (req, res) => {
     title: 'Handlebars playground',
     article: {
       title: 'Examples of templating with handlebars'
-    }
+    },
+    people: [
+      {firstName: "Block", lastName: "expressions"},
+      {firstName: "Define", lastName: "helpers"},
+      {firstName: "Change", lastName: "context"}
+    ]
   });
 })
 
